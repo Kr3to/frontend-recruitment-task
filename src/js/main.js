0fda696415772db42b8c53893866b45cbd292805
Kreto <mateusz.kret@zse.krakow.pl>
2022-08-30 23:08:30 +0200

    Refactored js&html code to be more reusable

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
 const overlay = document.querySelector('.popup__overlay')
 const content = document.querySelector('.popup__content')
-const btn = document.querySelector('.button')
-const closebtn = document.querySelector('.popup__close')
+const openButtons = document.querySelectorAll('[data-modal-target]')
+const closeButtons = document.querySelectorAll('[close-popup]')
 const resetbtn = document.querySelector('.count__reset')
 
 let clickCount = JSON.parse(localStorage.getItem('click'))
 
-const popupOpen = () =>{
+const openPopup = target => {
+    if (target == null) return
+    target.classList.add('active')
     overlay.classList.add('active')
-    content.classList.add('active')
     clickCount = clickCount + 1
     localStorage.setItem('click', JSON.stringify(clickCount))
     document.querySelector('.popup__count').textContent = clickCount
@@ -16,21 +17,38 @@ const popupOpen = () =>{
     if(clickCount >= 5) resetbtn.style.display = "block"
 }
 
-const popupClose = () =>{
+const closePopup = target => {
+    if (target == null) return
+    target.classList.remove('active')
     overlay.classList.remove('active')
-    content.classList.remove('active')
 }
 
-btn.addEventListener("click", popupOpen)
-
-content.addEventListener("click", e =>{
-    e.stopPropagation()
+openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const target = document.querySelector(button.dataset.modalTarget)
+        openPopup(target)
+    })
 })
 
-overlay.addEventListener("click", popupClose)
-closebtn.addEventListener("click", popupClose)
+content.addEventListener("click", e => {
+    e.stopPropagation()
+})
+  
+overlay.addEventListener('click', () => {   
+    const targets = document.querySelectorAll('.popup__content.active')
+        targets.forEach(target => {
+        closePopup(target)
+    })
+})
+  
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const target = button.closest('.target')
+        closePopup(target)
+    })
+})
 
-resetbtn.addEventListener("click", () =>{
+resetbtn.addEventListener("click", () => {
     localStorage.clear()
     window.location.reload()
 })
